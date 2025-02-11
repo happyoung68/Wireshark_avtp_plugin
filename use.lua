38d020a5fff35321702781d74c24f026dad93048
happyoung68 <1015120550@qq.com>
2025-02-11 23:01:16 +0800

    修改lua

--- use.lua
+++ use.lua
@@ -6,7 +6,6 @@
 
 AVTP_Protocol = Proto("Avtp", "Avtp Finder Protocol")     --avtp是协议名，会变成大写。那么avtp的小写默认为过滤器，第二个参数为协议说明，在wireshark查看具体信息时可以看到
 
-
 message_id = ProtoField.uint8("avtp.message_id", "MessageID as Filter", base.HEX)     --第一个参数为过滤器，第二个参数为显示包信息时的文字
 request_id     = ProtoField.uint8("avtp.requestid"     , "requestID"    , base.HEX)    
 response_to    = ProtoField.uint8("avtp.responseto"    , "responseTo"   , base.HEX)
@@ -18,11 +17,9 @@ function AVTP_Protocol.dissector(buffer, pinfo, tree)
   length = buffer:len()
   if length == 0 then return end
 
-  pinfo.cols.protocol = "Zhaixiyang"                                              --这个信息是显示在column上的信息
-
+  pinfo.cols.protocol = "use-avtp"                                              --这个信息是显示在column上的信息
 
   -- subtree:add(message_id, buffer(12,2))
-
 
   --local flag = buffer(12,2):uint()
   --if (flag == 0x433) then
@@ -35,21 +32,21 @@ function AVTP_Protocol.dissector(buffer, pinfo, tree)
 
   local decimal_timestamp = tonumber(hex_timestamp, 16)
 
--- 将时间戳单位从纳秒转换为秒
-local timestamp_in_seconds = decimal_timestamp / 1e9
+  -- 将时间戳单位从纳秒转换为秒
+  local timestamp_in_seconds = decimal_timestamp / 1e9
 
--- 分离秒和微秒部分
-local whole_seconds = math.floor(timestamp_in_seconds)
-local microseconds = (decimal_timestamp % 1e9) / 1e3  -- 从纳秒转换为微秒
+  -- 分离秒和微秒部分
+  local whole_seconds = math.floor(timestamp_in_seconds)
+  local microseconds = (decimal_timestamp % 1e9) / 1e3  -- 从纳秒转换为微秒
 
--- 使用 os.date 格式化秒部分
-local datetime = os.date("%Y-%m-%d %H:%M:%S", whole_seconds)
+  -- 使用 os.date 格式化秒部分
+  local datetime = os.date("%Y-%m-%d %H:%M:%S", whole_seconds)
 
--- 拼接微秒部分
-local full_datetime = string.format("%s.%06d", datetime, microseconds)
+  -- 拼接微秒部分
+  local full_datetime = string.format("%s.%06d", datetime, microseconds)
 
 
-local subtree = tree:add(AVTP_Protocol, buffer(), "Zhaixiyang's useful data" .. "-------------------------------------" .. "Message_ID:" .. buffer(12,2) .. "  CAN_Chn:" .. buffer(1,1):bitfield(0,8) + 1 .. "   Time:" .. full_datetime)    --这个是显示在包详细信息上得文字
+  local subtree = tree:add(AVTP_Protocol, buffer(), "Happyoung's useful data" .. "-------------------------------------" .. "Message_ID:" .. buffer(12,2) .. "  CAN_Chn:" .. buffer(1,1):bitfield(0,8) + 1 .. "   Time:" .. full_datetime)    --这个是显示在包详细信息上得文字
 
   -- 使用 os.date 格式化时间
   --local datetime = os.date("!%Y-%m-%d %H:%M:%S", timestamp_in_seconds)
@@ -61,19 +58,15 @@ local subtree = tree:add(AVTP_Protocol, buffer(), "Zhaixiyang's useful data" .. 
   --subtree = subtree:add(buffer(2,4),"The next two bytes")
   local flag = buffer(12,2):uint()
 
-elseif(flag == 1263)
-then
-  subtree:add(buffer(14, 64),"CCU_DTCCode: " .. buffer(14, 64):bitfield(7-(7%8), 512)*1+0 .. " " .. "[" .. getCCU_DTCCodeName(buffer(14, 64):bitfield(7-(7%8), 512)*1+0) .. "]")
-elseif(flag == 992)
-then
-  subtree:add(buffer(17, 1),"BHS_F_SOC: " .. buffer(17, 1):bitfield(7-(31%8), 8)*1+0 .. "% " .. "[" .. getBHS_F_SOCName(buffer(17, 1):bitfield(7-(31%8), 8)*1+0) .. "]")
-  subtree:add(buffer(18, 1),"BHS_F_SOH: " .. buffer(18, 1):bitfield(7-(39%8), 8)*1+0 .. "% " .. "[" .. getBHS_F_SOHName(buffer(18, 1):bitfield(7-(39%8), 8)*1+0) .. "]")
-
+  if(flag == 1263)
+  then
+    subtree:add(buffer(14, 64),"CCU_DTCCode: " .. buffer(14, 64):bitfield(7-(7%8), 512)*1+0 .. " " .. "[" .. getCCU_DTCCodeName(buffer(14, 64):bitfield(7-(7%8), 512)*1+0) .. "]")
+  elseif(flag == 992)
+  then
+    subtree:add(buffer(17, 1),"BHS_F_SOC: " .. buffer(17, 1):bitfield(7-(31%8), 8)*1+0 .. "% " .. "[" .. getBHS_F_SOCName(buffer(17, 1):bitfield(7-(31%8), 8)*1+0) .. "]")
+    subtree:add(buffer(18, 1),"BHS_F_SOH: " .. buffer(18, 1):bitfield(7-(39%8), 8)*1+0 .. "% " .. "[" .. getBHS_F_SOHName(buffer(18, 1):bitfield(7-(39%8), 8)*1+0) .. "]")
+  end
 end
-
-
-end
-
 
 dofile("ZXY_AVTP_Func.lua")
 
